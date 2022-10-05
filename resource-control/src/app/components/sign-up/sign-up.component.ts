0c96b5c8111cc2ac869fc96d7995695e0acd4210
zino-sage <mazinookaforc@gmail.com>
2022-10-05 12:43:07 +0100

    alive

--- resource-control/src/app/components/sign-up/sign-up.component.ts
+++ resource-control/src/app/components/sign-up/sign-up.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthenticationService, private router: Router ) { }
 
-  ngOnInit(): void {
+  signUpForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required),
+    confirmPassword: new FormControl('', Validators.required),
+  })
+
+  get name() {
+    return this.signUpForm.get('name')
   }
+  get email() {
+    return this.signUpForm.get('email')
+  }
+  get password() {
+    return this.signUpForm.get('password')
+  }
+  get confirmPassword() {
+    return this.signUpForm.get('confirmPassword')
+  } 
+  
+  ngOnInit(): void {
+  } 
 
+  submit() {
+    if (!this.signUpForm.valid) {
+      return;
+    }
+  }
 }
